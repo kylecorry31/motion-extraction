2ff129a9d03f14ad0f11db70b01b951f92aa8b5c
Kyle Corry <kylecorry31@gmail.com>
2024-04-21 17:48:28 -0400

    Option to draw diff over base

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 (function () {
-  let historyLength = 10;
+  let historyLength = 4;
   let history = [];
   const videoElement = document.querySelector("video");
   const outputImageElement = document.querySelector("#image");
+  let diffMultiplier = 1;
+  let baseMultiplier = 0;
 
   // An in memory canvas for reading the video frame
   const canvas = document.createElement("canvas");
@@ -15,6 +17,17 @@
   const input = document.querySelector("#history");
   input.addEventListener("input", (event) => {
     historyLength = parseInt(event.target.value);
+  });
+
+  // When the image is tapped, toggle between drawing the base and the diff only
+  outputImageElement.addEventListener("click", () => {
+    if (baseMultiplier === 0) {
+      diffMultiplier = 4;
+      baseMultiplier = 1;
+    } else {
+      diffMultiplier = 1;
+      baseMultiplier = 0;
+    }
   });
 
   // Open the webcam
@@ -38,12 +51,22 @@
         const firstFrame = history[0];
         const diff = context.createImageData(videoWidth, videoHeight);
         for (let i = 0; i < imageData.data.length; i += 4) {
-          diff.data[i] = Math.abs(imageData.data[i] - firstFrame.data[i]);
-          diff.data[i + 1] = Math.abs(
-            imageData.data[i + 1] - firstFrame.data[i + 1]
+          diff.data[i] = Math.min(
+            255,
+            Math.abs(imageData.data[i] - firstFrame.data[i]) * diffMultiplier +
+              baseMultiplier * imageData.data[i]
           );
-          diff.data[i + 2] = Math.abs(
-            imageData.data[i + 2] - firstFrame.data[i + 2]
+          diff.data[i + 1] = Math.min(
+            255,
+            Math.abs(imageData.data[i + 1] - firstFrame.data[i + 1]) *
+              diffMultiplier +
+              baseMultiplier * imageData.data[i + 1]
+          );
+          diff.data[i + 2] = Math.min(
+            255,
+            Math.abs(imageData.data[i + 2] - firstFrame.data[i + 2]) *
+              diffMultiplier +
+              baseMultiplier * imageData.data[i + 2]
           );
           diff.data[i + 3] = 255;
         }
